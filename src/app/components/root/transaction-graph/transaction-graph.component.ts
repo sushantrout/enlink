aac82f081280d9829909d3a5e0573cb6ab82114b
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-07-11 15:28:55 +0530

    fa icon implemnted

--- src/app/components/root/transaction-graph/transaction-graph.component.ts
+++ src/app/components/root/transaction-graph/transaction-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-transaction-graph',
@@ -6,31 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-graph.component.css'],
 })
 export class TransactionGraphComponent implements OnInit {
-  header = "Hedaer Demo"
-  zoom :boolean = false;
+  header = 'Hedaer Demo';
+  zoom: boolean = false;
   data: any = [];
-  graphType : string = "pie"
-  graphTypes = ["pie", "bar"];
+  graphType: string = 'pie';
+  graphTypes = ['pie', 'bar'];
+
+  @ViewChild('voltageChart') voltageChart: any;
   constructor() {}
 
   ngOnInit(): void {
     this.data = {
-      labels: ['A','B','C'],
+      labels: ['A', 'B', 'C'],
       datasets: [
-          {
-              data: [300, 50, 100],
-              backgroundColor: [
-                  "#42A5F5",
-                  "#66BB6A",
-                  "#FFA726"
-              ],
-              hoverBackgroundColor: [
-                  "#64B5F6",
-                  "#81C784",
-                  "#FFB74D"
-              ]
-          }
-      ]
-  };
+        {
+          data: [300, 50, 100],
+          backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726'],
+          hoverBackgroundColor: ['#64B5F6', '#81C784', '#FFB74D'],
+        },
+      ],
+    };
+  }
+  reinit(){
+    setTimeout(() => {
+      this.voltageChart.reinit();
+    }, 10);
   }
 }
