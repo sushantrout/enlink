b54797389eaeb16774e52f48cc9034ddfc8cf104
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-07-14 14:58:25 +0530

    search text remove after filter

--- src/app/components/root/latest-transaction/latest-transaction.component.ts
+++ src/app/components/root/latest-transaction/latest-transaction.component.ts
@@ -4,18 +4,23 @@ import { TransactionService } from 'src/app/serviceses/transaction.service';
 @Component({
   selector: 'app-latest-transaction',
   templateUrl: './latest-transaction.component.html',
-  styleUrls: ['./latest-transaction.component.css']
+  styleUrls: ['./latest-transaction.component.css'],
 })
 export class LatestTransactionComponent implements OnInit {
+  transactions!: any;
+  searchText: string = '';
 
-  transactions !:any;
-  constructor(private transactionService : TransactionService) { }
+  constructor(private transactionService: TransactionService) {}
 
   ngOnInit(): void {
+    this.searchText = '';
     this.transactions = [];
-    this.transactionService.get().subscribe((res : any) => {
+    this.transactionService.get().subscribe((res: any) => {
       this.transactions = res['success'];
     });
   }
-
+  clear(table: any) {
+    this.searchText = '';
+    table.clear();
+  }
 }
